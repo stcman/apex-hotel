e7275d0aa30f5b1fd888f676912d92efef444292
Tad Chifamba <37561906+stcman@users.noreply.github.com>
2021-07-11 01:46:22 -0400

    basic setup

---
+++ src/components/Splash.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="splash">
+    Welcome
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Splash',
+  components: {
+    
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Splash />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Splash from '../components/Splash.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Splash
   }
 }
 </script>
